9c5cdf4e2c701714994e75a224e335c534adffbd
Rohit Maheshwari <rohit.maheshwari@sjsu.edu>
2018-05-05 21:57:31 -0700

    Created Springboot basic structure
    Springboot boilerplate code

---
+++ freelancerbackend/src/test/java/com/example/freelancerbackend/FreelancerbackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.freelancerbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FreelancerbackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
