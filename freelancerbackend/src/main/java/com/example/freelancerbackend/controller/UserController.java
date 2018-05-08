6c4545882b9cc4780161771da1f1b43e57d098db
Rohit Maheshwari <rohit.maheshwari@sjsu.edu>
2018-05-08 00:53:48 -0700

    Upload Image API created
    Image is uploading at Static folder in Springboot

--- freelancerbackend/src/main/java/com/example/freelancerbackend/controller/UserController.java
+++ freelancerbackend/src/main/java/com/example/freelancerbackend/controller/UserController.java
@@ -11,10 +11,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.json.simple.JSONObject;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 
@@ -210,4 +215,26 @@ public class UserController {
 
         return new ResponseEntity(response,HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/saveProfile", method = RequestMethod.POST)
+    public String submit(@RequestParam("file") MultipartFile file, ModelMap modelMap, HttpSession session) {
+
+        String sessionUsername = session.getAttribute("username").toString();
+        System.out.println(sessionUsername);
+        try {
+            String current = new java.io.File( "." ).getCanonicalPath();
+
+            FileCopyUtils.copy(file.getBytes(), new File(current+"/src/main/resources/static/ProfileImage/" + sessionUsername+".jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+      //  String fileName = file.getOriginalFilename();
+        //modelMap.addAttribute("fileName", fileName);
+        return "fileupload";
+
+
+    }
+
+
+
 }
